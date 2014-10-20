79a02408ae37a5ea5bd3110925a590f52ea3950b
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-20 16:22:27 -0400

    gem file cleaned up, factory girl and pry added

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,16 @@
 class Reservation < ActiveRecord::Base
   belongs_to :restaurant
+
+  validates :availibility
+
+
+  private
+  def availibility
+    # restaurant
+    # start_time
+    # party_size
+    unless restaurant.available?(party_size, start_time)
+      errors.add(:base, 'Restaurant is full')
+    end
+  end
 end
