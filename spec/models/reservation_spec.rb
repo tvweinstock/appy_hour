e1a5c65d0cc3dbd6def3f8a5b9e6fc8325d221dc
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-20 22:07:59 -0400

    testing to ensure reservation does not exceed restaurant capacity

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe Reservation do
+  let!(:reservation) { FactoryGirl.create(:reservation) }
+
+  it "must not allow reservations beyond restaurant capacity" do
+    reservation.people = reservation.restaurant.capacity + 1
+    reservation.should_not be_valid
+  end
+
+  it "must allow reservations within restaurant capacity" do
+    reservations.people = [reservation.restaurant.capacity - 1, 1].max
+    reservation.should be_valid
+  end
 
   
 end
